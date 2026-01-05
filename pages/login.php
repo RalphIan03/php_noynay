ee0dc21141e51c46710c291a282d38c528ff7499
RalphIan <bluethunder3434@gmail.com>
2026-01-05 09:02:00 +0800

    new login and css

---
+++ pages/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ChaffGPT</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <div class="logincard">
+        <h1>Login</h1>
+        <form action="">
+            <label for="username">Username</label>
+            <input type="text" id="username" class="username" name="username">
+            <label for="password">Password</label>
+            <input type="password" id="password" class="password" name="password">
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+
+</body>
+
+</html>
