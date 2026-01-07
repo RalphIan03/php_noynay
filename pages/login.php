e0a0cd339e28dc3374d49be64febb3693da1c7a8
RalphIan <bluethunder3434@gmail.com>
2026-01-07 09:02:18 +0800

    update with login

--- pages/login.php
+++ pages/login.php
@@ -11,14 +11,33 @@
 <body>
     <div class="logincard">
         <h1>Login</h1>
-        <form action="">
+        <form action="functions/loginFunction.php" method="POST">
             <label for="username">Username</label>
             <input type="text" id="username" class="username" name="username">
             <label for="password">Password</label>
             <input type="password" id="password" class="password" name="password">
+            
             <button type="submit">Submit</button>
         </form>
     </div>
+
+    <?php
+
+    // $name = "john";
+    // $nums = "1";
+    // $num = 1.1111;
+
+    // echo ("hello world" . $name);
+    // echo ($num + $nums);
+
+    // var_dump($nums);
+    // var_dump($num);
+
+    // $num = 0;
+    // var_dump($num);
+
+    // echo $name . $nums + $num;
+    ?>
 
 </body>
 
