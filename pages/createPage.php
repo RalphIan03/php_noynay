02893dd50932374801f13a123602b5fa0e254325
RalphIan <bluethunder3434@gmail.com>
2026-01-15 09:09:56 +0800

    readyfordb

--- pages/createPage.php
+++ pages/createPage.php
@@ -1,16 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-    <?php
-    include "resources/header.php";
-    ?>
+<?php
+include "resources/header.php";
+?>
+
 <body>
     <?php
     include "resources/navigation.php";
     ?>
 
     <!-- contents -->
-     
-    <h1>This is Create page</h1>
+    <div class="useradd card">
+        <h1>Create User</h1>
+        <form action="functions/createUser.php" method="POST">
+            <label for="">Name</label>
+            <input type="text" name="user_name">
+            <label for="">Age</label>
+            <input type="number" name="user_age">
+            <label for="">Address</label>
+            <input type="text" name="user_address">
+
+            <button>Submit</button>
+        </form>
+    </div>
+
 
 </body>
 
